26e99341039a2db69ba0288ebb660f7b8d6486e8
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-02 23:18:24 +0700

    Update Routers.js

--- elearning_website/src/routers/Routers.js
+++ elearning_website/src/routers/Routers.js
@@ -1,24 +1,27 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom"
 
 
-import Home from '../pages/Home'
-import Course from '../pages/Course'
-import Favorite from '../pages/Favorite'
-import CourseDetails from '../pages/CourseDetails'
-import Checkout from '../pages/Checkout'
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
+import Home from "../pages/Home"
+import Course from "../pages/Course"
+import Saved from "../pages/Saved"
+import CourseDetails from "../pages/CourseDetails"
+import Checkout from "../pages/Checkout"
+import Login from "../pages/Login"
+import Signup from "../pages/Signup"
 
 const Routers = () => {
-  return <Routes>
-    <Route path='home' element={<Home/>} />
-    <Route path='course' element={<Course/>} />
-    <Route path='course/:id' element={<CourseDetails/>} />
-    <Route path='favorite' element={<Favorite/>} />
-    <Route path='checkout' element={<Checkout/>} />
-    <Route path='login' element={<Login/>} />
-    <Route path='signup' element={<Signup/>} />
+  return (
+    <Routes>
+    <Route path="/" element={<Navigate to="home" />} />
+    <Route path="home" element={<Home/>} />
+    <Route path="course" element={<Course/>} />
+    <Route path="course/:id" element={<CourseDetails/>} />
+    <Route path="saved" element={<Saved/>} />
+    <Route path="checkout" element={<Checkout/>} />
+    <Route path="login" element={<Login/>} />
+    <Route path="signup" element={<Signup/>} />
   </Routes>
+  );
 };
 
 export default Routers;
