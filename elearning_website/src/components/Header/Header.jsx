49af0f4939e390975492652cc8741183992ee6f6
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-02 23:17:58 +0700

    Update Header.jsx

--- elearning_website/src/components/Header/Header.jsx
+++ elearning_website/src/components/Header/Header.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+import "./header.css";
+
+import { motion } from "framer-motion";
+
+import logo from "../../assets/images/elearning-logo.png";
+import userIcon from "../../assets/images/user-icon.png";
+
+import { Container, Row } from "reactstrap";
+
+const nav_links = [
+  {
+    path: "home",
+    display: "Trang chủ",
+  },
+
+  {
+    path: "course",
+    display: "Các khóa học",
+  },
+
+  {
+    path: "saved",
+    display: "Mục yêu thích",
+  },
+];
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav_wrapper">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                <h1>E-P-DUNG</h1>
+                {/* <p>Since 1995</p> */}
+              </div>
+            </div>
 
-export default Header
+            <div className="navigation">
+              <ul className="menu">
+                {nav_links.map((item, index) => (
+                  <li className="nav_item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "nav_active" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="nav_icons">
+              <span className="sav_icon">
+                <i class="ri-heart-fill"></i>
+                <span className="badge">1</span>
+              </span>
+
+              <span className="set_icon">
+                <i class="ri-shopping-bag-fill"></i>
+                <span className="badge">1</span>
+              </span>
+
+              <span>
+                <motion.img whileTap={{ scale: 1.8 }} src={userIcon}    /*tạo hiệu ứng to nhỏ khi ấn vào nút tài khoản, sử dụng motion */
+                alt="" />
+              </span>
+            </div>
+
+            <div className="mobile_menu">
+              <span>
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
