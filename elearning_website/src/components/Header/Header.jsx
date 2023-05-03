5c8c36544bd29c961dd4413c4c9898a02d91b2f0
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-04 02:07:06 +0700

    Update Header.jsx

--- elearning_website/src/components/Header/Header.jsx
+++ elearning_website/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 import { NavLink } from "react-router-dom";
 import "./header.css";
@@ -9,7 +9,9 @@ import logo from "../../assets/images/elearning-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 
 import { Container, Row } from "reactstrap";
+import { useSelector } from "react-redux";
 
+// Đặt tên các mục trong menu chính ở giữa
 const nav_links = [
   {
     path: "home",
@@ -28,8 +30,36 @@ const nav_links = [
 ];
 
 const Header = () => {
+  /*Tạo hiệu ứng thanh menu di chuyển theo màn hình*/
+  const headerRef = useRef(null);
+
+  // tạo hiệu ứng thông báo số lượng khi đã thêm khóa học
+  const totalQuantity = useSelector(state=> state.saved.totalQuantity)
+
+  // const menuRef = useRef(null);
+
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky_header");
+      } else {
+        headerRef.current.classList.remove("sticky_header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    stickyHeaderFunc();
+
+    return () => window.removeEventListener("scroll", stickyHeaderFunc);
+  });
+  /*==============================================*/
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav_wrapper">
@@ -65,12 +95,17 @@ const Header = () => {
 
               <span className="set_icon">
                 <i class="ri-shopping-bag-fill"></i>
-                <span className="badge">1</span>
+                <span className="badge">{totalQuantity}</span>
               </span>
 
               <span>
-                <motion.img whileTap={{ scale: 1.8 }} src={userIcon}    /*tạo hiệu ứng to nhỏ khi ấn vào nút tài khoản, sử dụng motion */
-                alt="" />
+                <motion.img
+                  whileTap={{ scale: 1.8 }}
+                  src={
+                    userIcon
+                  } /*tạo hiệu ứng to nhỏ khi ấn vào nút tài khoản, sử dụng motion */
+                  alt=""
+                />
               </span>
             </div>
 
