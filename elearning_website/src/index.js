f6e2eb4959fccda8bcecd96529560e8e259ad1fb
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-04 02:07:14 +0700

    Update index.js

--- elearning_website/src/index.js
+++ elearning_website/src/index.js
@@ -6,11 +6,31 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+//Khai báo dữ liệu liên quan đến tạo redux
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* Triển khai redux */}
+      <Provider store={store}>
+        <ToastContainer
+          theme="light"
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+        />
+        {/* Same as */}
+        {/* <ToastContainer /> */}
+
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
