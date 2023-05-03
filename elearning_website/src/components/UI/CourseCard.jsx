4dc83b42246ecd46ecca210ccc9b028ad5076a24
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-04 02:07:10 +0700

    Update CourseCard.jsx

--- elearning_website/src/components/UI/CourseCard.jsx
+++ elearning_website/src/components/UI/CourseCard.jsx
@@ -5,25 +5,51 @@ import "../../styles/courses-card.css";
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const CourseCard = ({item}) => {
+import { toast } from "react-toastify";
+
+// Khai báo cho mục thuộc phần redux
+import { useDispatch } from "react-redux";
+import { savedActions } from "../../redux/slices/SavedSlice";
+
+const CourseCard = ({ item }) => {
+  // triển khai mục thuộc phần redux
+  const dispatch = useDispatch();
+
+  const addToSaved = () => {
+    dispatch(
+      savedActions.addItem({
+        id: item.id,
+        courseName: item.courseName,
+        price: item.price,
+        image: item.imgUrl,
+      })
+    );
+
+    toast.success("Khóa học đã được thêm vào mục Đã lưu");
+  };
+  // ===================================
+
   return (
     <Col lg="3" md="4" className="mb-2">
       <div className="course_item">
-        <div className="course_img"> 
-          <motion.img whileHover={{ scale: 1.1 }} src={item.imgUrl} alt="" />     {/* Tạo hiệu ứng khi di chuột vào khóa học */}
+        <div className="course_img">
+          <motion.img whileHover={{ scale: 1.1 }} src={item.imgUrl} alt="" />{" "}
+          {/* Tạo hiệu ứng khi di chuột vào khóa học */}
         </div>
         <div className="p-2 course_info">
-        <h3 className="course_name">
-          <Link to={`/shop/${item.id}`}>
-            {item.courseName}
-          </Link>
-        </h3>
-        <span>{item.category}</span>
+          <h3 className="course_name">
+            <Link to={`/shop/${item.id}`}>{item.courseName}</Link>
+          </h3>
+          <span>{item.category}</span>
         </div>
-        <div className="course_card-bottom 
-        d-flex align-items-center justify-content-between p-2">
+        <div
+          className="course_card-bottom 
+        d-flex align-items-center justify-content-between p-2"
+        >
           <span className="price">${item.price}</span>
-          <motion.span whileTap={{ scale: 1.2 }}>   {/* Tạo hiệu ứng khi ấn chuột vào nút thêm "+" */}
+          <motion.span whileTap={{ scale: 1.2 }} onClick={addToSaved}>
+            {" "}
+            {/* Tạo hiệu ứng khi ấn chuột vào nút thêm "+" */}
             <i className="ri-add-line"></i>
           </motion.span>
         </div>
