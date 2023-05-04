18a3ce713c750d84e4a329215ec89da3e43dd03d
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-04 12:24:55 +0700

    Update CourseDetails.jsx

--- elearning_website/src/pages/CourseDetails.jsx
+++ elearning_website/src/pages/CourseDetails.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+
+import { Container, Row, Col } from "reactstrap";
+import { useParams } from "react-router-dom";
+import courses from "../assets/data/products";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
 
 const CourseDetails = () => {
-  return (
-    <div>CourseDetails</div>
-  )
-}
+  const { id } = useParams();
+  const course = courses.find((item) => item.id == id);
 
-export default CourseDetails
+  const { imgUrl, courseName, price, avgRating, review, description } = course;
+
+  return (
+    <Helmet>
+      <CommonSection />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <img src={imgUrl} alt="" />
+            </Col>
+
+            <Col lg="6"></Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default CourseDetails;
