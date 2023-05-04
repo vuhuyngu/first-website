1ad930b5354706e56c2a5186e9cd7cec279739d5
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-04 12:24:45 +0700

    Update products.js

--- elearning_website/src/assets/data/products.js
+++ elearning_website/src/assets/data/products.js
@@ -94,28 +94,28 @@ const courses = [
     avgRating: 4.7,
   },
   
-  // {
-  //   id: "26",
-  //   courseName: "Rivet Bigelow Modern ",
-  //   imgUrl: productImg02,
-  //   category: "sofa",
-  //   price: 253,
-  //   shortDesc:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
-  //   reviews: [
-  //     {
-  //       rating: 4.8,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //     {
-  //       rating: 4.8,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //   ],
-  //   avgRating: 4.7,
-  // },
+  {
+    id: "26",
+    courseName: "Rivet Bigelow Modern ",
+    imgUrl: productImg02,
+    category: "sofa",
+    price: 253,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
 
   {
     id: "04",
@@ -140,51 +140,51 @@ const courses = [
     avgRating: 4.7,
   },
 
-  // {
-  //   id: "05",
-  //   courseName: "Faux Velvet Sofa",
-  //   imgUrl: productImg05,
-  //   category: "sofa",
-  //   price: 163,
-  //   shortDesc:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
-  //   reviews: [
-  //     {
-  //       rating: 4.6,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //     {
-  //       rating: 4.9,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //   ],
-  //   avgRating: 4.7,
-  // },
+  {
+    id: "05",
+    courseName: "Faux Velvet Sofa",
+    imgUrl: productImg05,
+    category: "sofa",
+    price: 163,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
 
-  // {
-  //   id: "06",
-  //   courseName: "Fllufy Sheep Sofa",
-  //   imgUrl: productImg06,
-  //   category: "sofa",
-  //   price: 163,
-  //   shortDesc:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
-  //   reviews: [
-  //     {
-  //       rating: 4.6,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //     {
-  //       rating: 4.9,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //   ],
-  //   avgRating: 4.7,
-  // },
+  {
+    id: "06",
+    courseName: "Fllufy Sheep Sofa",
+    imgUrl: productImg06,
+    category: "sofa",
+    price: 163,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
 
   {
     id: "07",
@@ -209,28 +209,28 @@ const courses = [
     avgRating: 4.7,
   },
 
-  // {
-  //   id: "27",
-  //   courseName: "Modern Arm Sofa",
-  //   imgUrl: productImg007,
-  //   category: "sofa",
-  //   price: 173,
-  //   shortDesc:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
-  //   reviews: [
-  //     {
-  //       rating: 4.6,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //     {
-  //       rating: 4.9,
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     },
-  //   ],
-  //   avgRating: 4.7,
-  // },
+  {
+    id: "27",
+    courseName: "Modern Arm Sofa",
+    imgUrl: productImg007,
+    category: "sofa",
+    price: 173,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    reviews: [
+      {
+        rating: 4.6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        rating: 4.9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
+    avgRating: 4.7,
+  },
 
   {
     id: "08",
