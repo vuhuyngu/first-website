7bb679ec54ed621ef359b51a735e239e337f4aa8
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-05 11:03:46 +0700

    Update Signup.jsx

--- elearning_website/src/pages/Signup.jsx
+++ elearning_website/src/pages/Signup.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+
+import "../styles/login.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [file, setFile] = useState(null);
+
+  return (
+    <Helmet title="Đăng kí">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h3 className="fw-bold mb-3">Đăng kí</h3>
+
+              <Form className="auth_form">
+                <FormGroup className="form_group">
+                  <input
+                    type="text"
+                    placeholder="Tên người dùng"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </FormGroup>
+
+                <FormGroup className="form_group">
+                  <input
+                    type="email"
+                    placeholder="Nhập email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+
+                <FormGroup className="form_group">
+                  <input
+                    type="password"
+                    placeholder="Nhập mật khẩu"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+
+                <FormGroup className="form_group">
+                  <input
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                </FormGroup>
+
+                <button type="submit" className="buy_btn auth_btn">
+                  Đăng kí tài khoản mới
+                </button>
+                <p>
+                  Đã có tài khoản? <Link to="/login">Đăng nhập</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Signup;
