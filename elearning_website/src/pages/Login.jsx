e2d89d24caf4323477c21208e3f90af6ee04c58d
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-05 11:03:45 +0700

    Update Login.jsx

--- elearning_website/src/pages/Login.jsx
+++ elearning_website/src/pages/Login.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+
+import "../styles/login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Helmet title="Đăng nhập">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h3 className="fw-bold mb-3">Đăng nhập</h3>
+
+              <Form className="auth_form">
+                <FormGroup className="form_group">
+                  <input
+                    type="email"
+                    placeholder="Nhập email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+
+                <FormGroup className="form_group">
+                  <input
+                    type="password"
+                    placeholder="Nhập mật khẩu"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+
+                <button type="submit" className="buy_btn auth_btn">
+                  Đăng nhập
+                </button>
+                <p>
+                  Chưa có tài khoản? <Link to="/signup">Tạo tài khoản</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Login;
