b594879076cfcfec20c56291bd377e6547c75e0d
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-07 10:07:41 +0700

    Update Signup.jsx

--- elearning_website/src/pages/Signup.jsx
+++ elearning_website/src/pages/Signup.jsx
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { setDoc, doc } from "firebase/firestore";
 import { auth } from "../firebase.config";
+import { storage } from "../firebase.config";
+import { db } from "../firebase.config";
+
+import { toast } from "react-toastify";
 
 import "../styles/login.css";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const [username, setUsername] = useState("");
@@ -13,6 +21,8 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const signup = async (e) => {
     e.preventDefault();
@@ -26,8 +36,40 @@ const Signup = () => {
       );
 
       const user = userCredential.user;
-      console.log(user);
-    } catch (error) {}
+
+      const storageRef = ref(storage, `images/${Date.now() + username}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          toast.error(error.message);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            // update user profile
+            await updateProfile(user, {
+              displayName: username,
+              photoURL: downloadURL,
+            });
+
+            // store user data in firestore database
+            await setDoc(doc(db, "users", user.uid), {
+              uid: user.uid,
+              displayName: username,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+
+      setLoading(false);
+      toast.success("Account created");
+      navigate("/login");
+    } catch (error) {
+      setLoading(false);
+      toast.error("Đã xảy ra sự cố");
+    }
   };
 
   return (
@@ -35,52 +77,58 @@ const Signup = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="6" className="m-auto text-center">
-              <h3 className="fw-bold mb-3">Đăng kí</h3>
+            {loading ? (
+              <Col lg="12" className="text-center">
+                <h5 className="fw-bold">Loading...</h5>
+              </Col>
+            ) : (
+              <Col lg="6" className="m-auto text-center">
+                <h3 className="fw-bold mb-3">Đăng kí</h3>
 
-              <Form className="auth_form" onSubmit={signup}>
-                <FormGroup className="form_group">
-                  <input
-                    type="text"
-                    placeholder="Tên người dùng"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </FormGroup>
+                <Form className="auth_form" onSubmit={signup}>
+                  <FormGroup className="form_group">
+                    <input
+                      type="text"
+                      placeholder="Tên người dùng"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </FormGroup>
 
-                <FormGroup className="form_group">
-                  <input
-                    type="email"
-                    placeholder="Nhập email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </FormGroup>
+                  <FormGroup className="form_group">
+                    <input
+                      type="email"
+                      placeholder="Nhập email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormGroup>
 
-                <FormGroup className="form_group">
-                  <input
-                    type="password"
-                    placeholder="Nhập mật khẩu"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </FormGroup>
+                  <FormGroup className="form_group">
+                    <input
+                      type="password"
+                      placeholder="Nhập mật khẩu"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </FormGroup>
 
-                <FormGroup className="form_group">
-                  <input
-                    type="file"
-                    onChange={(e) => setFile(e.target.files[0])}
-                  />
-                </FormGroup>
+                  <FormGroup className="form_group">
+                    <input
+                      type="file"
+                      onChange={(e) => setFile(e.target.files[0])}
+                    />
+                  </FormGroup>
 
-                <button type="submit" className="buy_btn auth_btn">
-                  Đăng kí tài khoản mới
-                </button>
-                <p>
-                  Đã có tài khoản? <Link to="/login">Đăng nhập</Link>
-                </p>
-              </Form>
-            </Col>
+                  <button type="submit" className="buy_btn auth_btn">
+                    Đăng kí tài khoản mới
+                  </button>
+                  <p>
+                    Đã có tài khoản? <Link to="/login">Đăng nhập</Link>
+                  </p>
+                </Form>
+              </Col>
+            )}
           </Row>
         </Container>
       </section>
